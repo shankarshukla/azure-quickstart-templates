d85368787dc48edf33abb1b0fbcac531f79a8fee
Shankar <shankar.shukla@gmail.com>
2021-01-25 12:49:36 -0800

    Update install-elasticsearch-v7.sh

--- elasticsearch-vmss/install-elasticsearch-v7.sh
+++ elasticsearch-vmss/install-elasticsearch-v7.sh
@@ -322,7 +322,7 @@ install_nginx()
 	sudo htpasswd -b -c /etc/nginx/htpasswd.users readonly $READONLY_PASSWORD
 	
 	log "########## configure nginx #########"
-	echo "readonly:$readonlyPassword">>tempt.txt
+	echo 'readonly:'$READONLY_PASSWORD>>tempt.txt
 	
 	encodedPassword=`base64 tempt.txt`
 	echo "$encodedPassword"
